a067494063fda27b6dea1f34b504131e9b6b4061
DigitalDeltaGaming <contact@digitaldeltagaming.net>
2025-07-28 03:05:06 -0400

    staged

--- server.js
+++ server.js
@@ -89,8 +89,8 @@ if (!fs.existsSync(path.join(__dirname, 'data'))) {
 
 // Steam Passport Strategy
 passport.use(new SteamStrategy({
-    returnURL: `http://34.72.229.75:${PORT}/auth/steam/return`,
-    realm: `http://34.72.229.75:${PORT}/`,
+    returnURL: `http://34.63.247.63:${PORT}/auth/steam/return`,
+    realm: `http://34.63.247.63:${PORT}/`,
     apiKey: STEAM_API_KEY
 }, (identifier, profile, done) => {
     // Extract Steam ID from identifier
